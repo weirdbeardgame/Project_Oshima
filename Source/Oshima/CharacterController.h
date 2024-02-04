cc15ec518ea6636e889012b30d68b2fcfc28d367
Timothy O'Barr <finalfantasytimothy@gmail.com>
2024-02-03 17:13:13 -0700

    Work on climbing logic
    Added movement mode
    Added climbable property
    Added Stubb physics function.
    Added blueprint callable on Health.
    Added Collision component.
    
    Changed to Custom movmement component Move function.
    Added adjustable variables for walk, backwalk and rotation speeds in blueprint
    Added custom rotation physics though for now pawn rotation is tied to camera rotation.
    Added crouching and prone crouched movment
    
    Work on DetectLedges:
    Accurately detect walls
    Added debug draw and cleaned up old levels
    Added stubs for Ledge detect properties that can be spawned in blueprints.
    
    Added LedgeProperties struct
    Added SweepMultiple function to shoot multiple spheres out and detect ledges of varying heights

--- Source/Oshima/CharacterController.h
+++ Source/Oshima/CharacterController.h
@@ -9,30 +9,42 @@
 #include "Camera/CameraComponent.h"
 #include "ActorComponents/Health.h"
 #include "ActorComponents/ActorType.h"
+#include "ActorComponents/TankControls.h"
+#include "ActorComponents/CollisionDetect.h"
 #include "Weapon.h"
 #include "CharacterController.generated.h"
 
+
+UENUM()
+enum class StateMachine {LADDER, NORMAL, LOW_HEALTH, DEAD, SOUNDJACKING};
 
 UCLASS(config = Game)
 class OSHIMA_API ACharacterController : public ACharacter
 {
 	GENERATED_BODY()
 
+	// Input mapping stuffs
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 		class UInputAction* MoveAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 		class UInputAction* LookAction;
 
-	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+		class UInputAction* CrouchAction;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 		class UInputMappingContext* DefaultMappingContext;
 
+	// Status and other objects	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	AWeapon *currentWeapon;
+		AWeapon *currentWeapon;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
-		class UHealth* health;
+		class UHealth* health;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+		class UCollisionDetect* collision;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
 		class UActorType* tag;
@@ -40,6 +52,10 @@ class OSHIMA_API ACharacterController : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
 		FString CharacterName;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		StateMachine currentState;
+
+	// Camera stuff
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* CameraComp;
 
@@ -49,15 +65,23 @@ class OSHIMA_API ACharacterController : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 		class USpringArmComponent* SpringArmComp;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+        FLedgeProperties LedgeProperties;
+
+
 public:
 	// Sets default values for this character's properties
-	ACharacterController();
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) 
-	bool jacking;
+	ACharacterController(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool jacking;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly)
+	UTankControls* MovementComponent;
 
 public:	
 	// Called every frame
@@ -66,8 +90,21 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE UTankControls* GetCustomCharacterMovement() const { return MovementComponent; }
+
 	void Move(const FInputActionValue& Value);
 	void Look(const FInputActionValue& Value);
+
+	void GoProne(const FInputActionValue& Value);
+
+	bool DetectWall(FVector Forward, FVector CurrentPosition, ECollisionChannel CollisionChan);
+
+	UFUNCTION(BlueprintCallable)
+	bool DetectLedge(FVector Forward, FVector CurrentPosition, ECollisionChannel CollisionChan);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateState(TEnumAsByte <StateMachine> s);
 
 	UFUNCTION(BlueprintNativeEvent)
 	void EquipWeapon(AWeapon* weap);
